ca14a390e102e19a332cd857bc4c7bcdcb9405d8
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-03 21:24:35 +0600

    Add wrap all the routes inside Layout component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 
+import Layout from './components/Layout/Layout';
 import HomePage from './pages/Homepage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
@@ -7,12 +8,14 @@ import PageNotFound from './pages/PageNotFound';
 
 const App = () => {
   return (
+  <Layout>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/contact" element={<ContactPage />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
+  </Layout>
   )
 }
 
