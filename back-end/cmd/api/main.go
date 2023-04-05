a5f21cc3a0993d8b565ac316aa02f6c7ec8d33f2
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-05 20:00:41 +0600

    Install PosgreSQL Driver and connect with database

--- back-end/cmd/api/main.go
+++ back-end/cmd/api/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/raihan2bd/filmwise/models"
 )
 
@@ -38,6 +42,14 @@ type application struct {
 func main() {
 	var cfg config
 
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
@@ -50,4 +62,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- back-end/go.mod
+++ back-end/go.mod
@@ -1,3 +1,5 @@
 module github.com/raihan2bd/filmwise
 
 go 1.20
+
+require github.com/lib/pq v1.10.7 // indirect
