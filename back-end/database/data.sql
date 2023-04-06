023118e0d810b04420fb5152ffb17c1493c19601
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-06 20:32:42 +0600

    Add dummy data into the genres table

---
+++ back-end/database/data.sql
@@ -0,0 +1,13 @@
+-- Insert dummy data into the genres table
+insert into genres(genre_name, created_at, updated_at)
+  values
+    ('Drama', '2023-04-06', '2023-04-06'),
+    ('Crime', '2023-04-06', '2023-04-06'),
+    ('Action', '2023-04-06', '2023-04-06'),
+    ('Comic Book', '2023-04-06', '2023-04-06'),
+    ('Sci-Fi', '2023-04-06', '2023-04-06'),
+    ('Mystery', '2023-04-06', '2023-04-06'),
+    ('Adventure', '2023-04-06', '2023-04-06'),
+    ('Comedy', '2023-04-06', '2023-04-06'),
+    ('Romance', '2023-04-06', '2023-04-06');
+
