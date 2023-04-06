69cf53df28a605b91ae43718687446ea5c7d82cd
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-06 21:58:06 +0600

    Add dummy data to the movies table

--- back-end/database/data.sql
+++ back-end/database/data.sql
@@ -11,3 +11,10 @@ insert into genres(genre_name, created_at, updated_at)
     ('Comedy', '2023-04-06', '2023-04-06'),
     ('Romance', '2023-04-06', '2023-04-06');
 
+-- Insert dummy data into the movies table
+insert into movies (title, description, release_date, runtime, rating, created_at, updated_at)
+  values
+    ('The Shawshank Redemption', 'Two imprisoned men bond over a number of years', '1994-10-14', 142, 9.3, '2023-04-06', '2023-04-06'),
+    ('The Pursuit of Happyness', 'Based on a true story about a man named Christopher Gardner.', '2006-12-15', 117, 8.0, '2023-04-06', '2023-04-06'),
+    ('The Dark Knight', 'The menace known as the Joker wreaks havoc on Gotham City.', '2008-07-18', 152, 9.0, '2023-04-06', '2023-04-06'),
+    ('Forrest Gump', 'Forrest Gump is a simple man with a low I.Q. but good intentions.', '1994-07-06', 142, 8.8, '2023-04-06', '2023-04-06');
