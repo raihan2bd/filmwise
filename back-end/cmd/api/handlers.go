ffcd6a3b7a88ed542f84e8cba65bb2077ac9c209
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-07 22:16:26 +0600

    Add get all movies by genres feature inside the back-end

--- back-end/cmd/api/handlers.go
+++ back-end/cmd/api/handlers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) GetStatus(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +32,27 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+}
+
+// Get all movies by genre
+func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	genreID, err := strconv.Atoi(params.ByName("genre_id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	movies, err := app.models.DB.GetAllMovies(genreID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/status", app.GetStatus)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 
 	return app.enableCORS(router)
 }
