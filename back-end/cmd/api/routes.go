c21694d32694bad9e4f4e6fc7bfda3b92e21ac62
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-07 22:01:54 +0600

    Add get all movies route to the back-end

--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -10,6 +10,7 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.GetStatus)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 
 	return app.enableCORS(router)
 }
