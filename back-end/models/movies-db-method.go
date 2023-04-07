ae3c46ccee227896a5cf876d42d71447f37602e8
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-07 22:02:56 +0600

    Add database method for get all movies with genre

---
+++ back-end/models/movies-db-method.go
@@ -0,0 +1,78 @@
+package models
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func (m *DBModel) GetAllMovies(genre ...int) ([]*Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	where := ""
+	if len(genre) > 0 {
+		where = fmt.Sprintf("where id in (select movie_id from movies_genres where genre_id = %d)", genre[0])
+	}
+
+	query := fmt.Sprintf("select id, title, description, year, release_date, rating, runtime, created_at, updated_at from movies  %s order by title", where)
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var movies []*Movie
+	for rows.Next() {
+		var movie Movie
+		err = rows.Scan(
+			&movie.ID,
+			&movie.Title,
+			&movie.Description,
+			&movie.Year,
+			&movie.ReleaseDate,
+			&movie.Rating,
+			&movie.Runtime,
+			&movie.CreatedAt,
+			&movie.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// get genres, if any
+		genreQuery := `select
+			mg.id, mg.movie_id, mg.genre_id, g.genre_name
+		from
+			movies_genres mg
+			left join genres g on (g.id = mg.genre_id)
+		where
+			mg.movie_id = $1
+		`
+
+		genreRows, _ := m.DB.QueryContext(ctx, genreQuery, movie.ID)
+
+		genres := make(map[int]string)
+		for genreRows.Next() {
+			var mg MovieGenre
+			err := genreRows.Scan(
+				&mg.ID,
+				&mg.MovieID,
+				&mg.GenreID,
+				&mg.Genre.GenreName,
+			)
+			if err != nil {
+				return nil, err
+			}
+			genres[mg.GenreID] = mg.Genre.GenreName
+		}
+		genreRows.Close()
+
+		movie.MovieGenre = genres
+		movies = append(movies, &movie)
+	}
+
+	return movies, nil
+}
