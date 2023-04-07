9641677e1b383099ab8280d02a1bf428a5f35727
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-07 22:03:47 +0600

    Add get all movies handlers inside handlers.go file

--- back-end/cmd/api/handlers.go
+++ back-end/cmd/api/handlers.go
@@ -15,3 +15,18 @@ func (app *application) GetStatus(w http.ResponseWriter, r *http.Request) {
 		app.logger.Println(err)
 	}
 }
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.GetAllMovies()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+}
