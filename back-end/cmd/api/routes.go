79efb6abac7840f3147757e18158f8ad202da11c
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-09 00:43:42 +0600

    Add extra route for filter movies

--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/status", app.GetStatus)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+	router.HandlerFunc(http.MethodGet, "/v2/movies", app.getAllMoviesByFilter)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
