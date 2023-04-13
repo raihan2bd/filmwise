19eadb7f2c0d2e8f060849ce2ac5721ad651a55c
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-13 23:59:37 +0600

    Add insert new movie functionality to the handlers

--- back-end/cmd/api/handlers.go
+++ back-end/cmd/api/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/raihan2bd/filmwise/models"
@@ -16,6 +17,17 @@ const (
 	defaultPage    = 1
 	defaultPerPage = 3
 )
+
+type MoviePayload struct {
+	ID          string         `json:"id"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Year        string         `json:"year"`
+	ReleaseDate string         `json:"release_date"`
+	Runtime     string         `json:"runtime"`
+	Rating      string         `json:"rating"`
+	MovieGenre  map[int]string `json:"genres"`
+}
 
 func (app *application) GetStatus(w http.ResponseWriter, r *http.Request) {
 	currentStatus := AppStatus{
@@ -129,32 +141,86 @@ func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
 
 // AddNewMovie will insert a new movie
 func (app *application) AddNewMovie(w http.ResponseWriter, r *http.Request) {
-	var movie models.Movie
+	var payload MoviePayload
 
 	// read json from the body
-	err := app.readJSON(w, r, &movie)
+	err := app.readJSON(w, r, &payload)
 	if err != nil {
 		app.badRequest(w, r, err)
 		return
 	}
 
-	validation := validator.New()
-	validation.IsLength(movie.Title, "title", "title should be minimum 3 to 255 characters long!", 3, 255)
+	validator := validator.New()
+	validator.IsLength(payload.Title, "title", 3, 255)
+	validator.IsLength(payload.Description, "description", 20, 500)
+	validator.Required(payload.Year, "year", "year is required")
+	validator.Required(payload.ReleaseDate, "release_date", "release_date is required")
 
-	if validation.Valid() {
-		app.logger.Println("form is validated")
-	} else {
-		// read json from the body
-		err := app.writeJSON(w, http.StatusBadRequest, validation)
+	year, err := strconv.Atoi(payload.Year)
+	if err != nil {
+		validator.AddError("year", "invalid year!")
+	}
+
+	releaseDate, err := time.Parse("2006-01-02", payload.ReleaseDate)
+	if err != nil {
+		validator.AddError("release_date", "invalid release_date!")
+	}
+
+	runtime, err := strconv.Atoi(payload.Runtime)
+	if err != nil {
+		validator.AddError("runtime", "invalid runtime!")
+	}
+
+	rating, err := strconv.ParseFloat(payload.Rating, 64)
+	if err != nil {
+		validator.AddError("rating", "invalid rating!")
+	}
+
+	if len(payload.MovieGenre) <= 0 {
+		validator.AddError("genres", "movie genre is required")
+	}
+
+	if len(payload.MovieGenre) > 5 {
+		validator.AddError("genres", "maximum 5 genres are allowed")
+	}
+
+	if !validator.Valid() {
+		err := app.writeJSON(w, http.StatusBadRequest, validator)
 		if err != nil {
 			app.badRequest(w, r, err)
 			return
 		}
-		app.logger.Println("for isn't validated")
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	var movie models.Movie
+	movie.Title = strings.Trim(payload.Title, "")
+	movie.Description = strings.Trim(payload.Description, "")
+	movie.Year = year
+	movie.ReleaseDate = releaseDate
+	movie.Runtime = runtime
+	movie.Rating = rating
+	movie.MovieGenre = payload.MovieGenre
+
+	movieID, moviesGenres, err := app.models.DB.InsertMovie(&movie)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var resp struct {
+		Error        bool           `json:"error"`
+		ID           int            `json:"id"`
+		MoviesGenres map[int]string `json:"movies_genres"`
+		Message      string         `json:"message"`
+	}
+
+	resp.Error = false
+	resp.ID = movieID
+	resp.MoviesGenres = moviesGenres
+	resp.Message = "Movie is inserted successfully!"
+
+	err = app.writeJSON(w, http.StatusOK, resp)
 	if err != nil {
 		app.errorJSON(w, err)
 	}
