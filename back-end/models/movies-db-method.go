f7603fd6d4588beddbb3e6a0ebfa1096a3fb6698
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-14 23:15:17 +0600

    Add necessary database method to get and delete one movie from the Database

--- back-end/models/movies-db-method.go
+++ back-end/models/movies-db-method.go
@@ -278,7 +278,7 @@ func (m *DBModel) InsertMovie(movie *Movie) (int, map[int]string, error) {
 		return movieID, movieGenres, errors.New("invalid movie data! failed to save the movie")
 	}
 
-	for key, _ := range movieGenres {
+	for key := range movieGenres {
 		stmt = `insert into movies_genres ( genre_id, movie_id, created_at, updated_at)
 						values($1, $2, $3, $4)`
 		_, err = m.DB.ExecContext(ctx, stmt, key, movieID, time.Now(), time.Now())
@@ -287,4 +287,79 @@ func (m *DBModel) InsertMovie(movie *Movie) (int, map[int]string, error) {
 		}
 	}
 	return movieID, movieGenres, nil
+}
+
+// Get returns one movie and error, if any
+func (m *DBModel) Get(id int) (*Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `select id, title, description, year, release_date, rating, runtime, mpaa_rating,
+				created_at, updated_at from movies where id = $1
+	`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var movie Movie
+
+	err := row.Scan(
+		&movie.ID,
+		&movie.Title,
+		&movie.Description,
+		&movie.Year,
+		&movie.ReleaseDate,
+		&movie.Rating,
+		&movie.Runtime,
+		&movie.CreatedAt,
+		&movie.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// get genres, if any
+	query = `select
+				mg.id, mg.movie_id, mg.genre_id, g.genre_name
+			from
+				movies_genres mg
+				left join genres g on (g.id = mg.genre_id)
+			where
+				mg.movie_id = $1
+	`
+
+	rows, _ := m.DB.QueryContext(ctx, query, id)
+	defer rows.Close()
+
+	genres := make(map[int]string)
+	for rows.Next() {
+		var mg MovieGenre
+		err := rows.Scan(
+			&mg.ID,
+			&mg.MovieID,
+			&mg.GenreID,
+			&mg.Genre.GenreName,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres[mg.ID] = mg.Genre.GenreName
+	}
+
+	movie.MovieGenre = genres
+
+	return &movie, nil
+}
+
+func (m *DBModel) DeleteMovie(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := "delete from movies where id = $1"
+
+	_, err := m.DB.ExecContext(ctx, stmt, id)
+	if err != nil {
+		return errors.New("failed to delete the movie")
+	}
+
+	return nil
 }
