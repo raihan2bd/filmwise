8d35ef690474ccb999065f17a0343922748a8dd9
Abu Raihan <raihan2bd.official@gmail.com>
2023-04-14 23:13:05 +0600

    Add get one movie route inside the back-end

--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -13,10 +13,11 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMoviesByFilter)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/genre/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 
 	// private routes for admin
 	router.HandlerFunc(http.MethodPost, "/v1/admin/movie/add", app.AddNewMovie)
-	router.HandlerFunc(http.MethodPut, "/v1/admin/movie/edit/:id", app.AddNewMovie)
+	router.HandlerFunc(http.MethodPut, "/v1/admin/movie/edit", app.AddNewMovie)
 
 	return app.enableCORS(router)
 }
