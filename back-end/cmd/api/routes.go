1212666081353af23cc55e329da7bd573a6c1a50
raihan2bd <raihan2bd.official@gmail.com>
2023-05-03 19:25:39 +0600

    Secure admin private routes

--- back-end/cmd/api/routes.go
+++ back-end/cmd/api/routes.go
@@ -25,6 +25,9 @@ func (app *application) routes() http.Handler {
 
 	// initialize secure middleware
 	secure := alice.New(app.authenticate)
+
+	// initialize admin middleware
+	secureAdmin := alice.New(app.adminAuth)
 	// public routes
 	router.HandlerFunc(http.MethodGet, "/status", app.GetStatus)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMoviesByFilter)
@@ -35,30 +38,33 @@ func (app *application) routes() http.Handler {
 
 	// router.HandlerFunc(http.MethodPost, "/v1/user/signup/", app.signUp)
 	router.HandlerFunc(http.MethodPost, "/v1/user/login/", app.loginUser)
-	// router.HandlerFunc(http.MethodGet, "/v1/user/logout/", app.logout)
 
 	/* private routes for user */
 
 	// routes for ratings
 	router.POST("/v1/rating/add", app.wrap(secure.ThenFunc(app.addOrUpdateRating)))
 
-	// routes for comments
-	router.HandlerFunc(http.MethodPost, "/v1/movie/comments/add", app.addOrUpdateComment)
-	router.HandlerFunc(http.MethodPut, "/v1/movie/comments/update", app.addOrUpdateComment)
-	router.HandlerFunc(http.MethodGet, "/v1/movie/comments/delete/:id", app.deleteComment)
+	// user private routes to manage comments
+	router.POST("/v1/movie/comments/add", app.wrap(secure.ThenFunc(app.addOrUpdateComment)))
+	router.PUT("/v1/movie/comments/update", app.wrap(secure.ThenFunc(app.addOrUpdateComment)))
+	router.GET("/v1/movie/comments/delete/:id", app.wrap(secure.ThenFunc(app.deleteComment)))
 
 	// routes for favorites
 	router.HandlerFunc(http.MethodGet, "/v1/movie/favorites/add/:id", app.addFavorite)
 	router.HandlerFunc(http.MethodGet, "/v1/movie/favorites/delete/:id", app.removeFavorite)
 
 	// private routes for admin
-	router.HandlerFunc(http.MethodPost, "/v1/images/upload", app.uploadImage)
-	router.HandlerFunc(http.MethodPost, "/v1/admin/genre/add", app.addOrUpdateGenre)
-	router.HandlerFunc(http.MethodPut, "/v1/admin/genre/edit", app.addOrUpdateGenre)
-	router.HandlerFunc(http.MethodGet, "/v1/admin/genre/delete/:id", app.deleteGenre)
-	router.HandlerFunc(http.MethodPost, "/v1/admin/movie/add", app.AddNewMovie)
-	router.HandlerFunc(http.MethodPut, "/v1/admin/movie/edit", app.AddNewMovie)
-	router.HandlerFunc(http.MethodGet, "/v1/admin/movie/delete/:id", app.deleteMovie)
+	router.POST("/v1/images/upload", app.wrap(secureAdmin.ThenFunc(app.uploadImage)))
+
+	// admin routes for manage movie genre
+	router.POST("/v1/admin/genre/add", app.wrap(secureAdmin.ThenFunc(app.addOrUpdateGenre)))
+	router.PUT("/v1/admin/genre/edit", app.wrap(secureAdmin.ThenFunc(app.addOrUpdateGenre)))
+	router.GET("/v1/admin/genre/delete/:id", app.wrap(secureAdmin.ThenFunc(app.deleteGenre)))
+
+	// admin routes to manage movie
+	router.POST("/v1/admin/movie/add", app.wrap(secureAdmin.ThenFunc(app.AddNewMovie)))
+	router.PUT("/v1/admin/movie/edit", app.wrap(secureAdmin.ThenFunc(app.AddNewMovie)))
+	router.GET("/v1/admin/movie/delete/:id", app.wrap(secureAdmin.ThenFunc(app.deleteMovie)))
 
 	return app.enableCORS(router)
 }
