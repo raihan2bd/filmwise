8181aea8a4f73c5272deff9d1c559453fd6ec5c0
raihan2bd <raihan2bd.official@gmail.com>
2023-05-13 12:35:47 +0600

    Add movies page layout

--- front-end/src/pages/MoviesPage.jsx
+++ front-end/src/pages/MoviesPage.jsx
@@ -1,5 +1,28 @@
+import classes from './MoviesPage.module.css'
+
 const MoviesPage = () => (
-  <h2>Hello From About Page!</h2>
+  <section>
+    <div className={classes.movies}>
+    <div className={classes.movies_header}>
+      <h2>Movies</h2>
+      <button button>Filter</button>
+    </div>
+      <ul className={classes.movies_list}>
+        <li className={classes.movie}>
+          <div className={classes.movie_img}>
+            <img src="" alt="movie"></img>
+          </div>
+          <a href="#" className={classes.movie_title}></a>
+          <div className={classes.movie_short_info}>
+            <button className={classes.favorite}>Fav</button>
+            <div className={classes.rating}>
+              <span>8.5</span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 );
 
 export default MoviesPage;
